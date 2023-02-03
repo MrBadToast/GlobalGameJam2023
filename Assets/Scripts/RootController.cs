9032654c7275cc852baeee474779be0c4328e0bc
Netckle <33153152+Netckle@users.noreply.github.com>
2023-02-04 05:40:47 +0900

    mid term

--- Assets/Scripts/RootController.cs
+++ Assets/Scripts/RootController.cs
@@ -10,10 +10,11 @@ using Unity.VisualScripting;
 
 using static UnityEditor.PlayerSettings;
 
-public class Root
+public class Root : MonoBehaviour
 {
-    public int index;
-    public RootContoller contoller;
+    public Tile root;
+    public Tile stem;
+    public Tile limit;
 
     public Vector2 startPosition;
     public Vector2 rootPosition;
@@ -21,9 +22,34 @@ public class Root
 
     private bool canInstall = false;
 
+    private Tilemap map;
+    private GridLayout gridLayout;
+    private Vector3Int cellPosition;
+
+    private void Start()
+    {
+        map = GetComponent<Tilemap>();
+        gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+    }
+
+    public bool IsTileCanInstall(Tile tile, Vector2 pos)
+    {
+        Tile target = map.GetTile<Tile>(gridLayout.WorldToCell(pos));
+        if (tile == target || tile == limit)
+            return false;
+        else
+            return true;
+    }
+
+    public void ChangeTile(Tile tile, Vector2 point)
+    {
+        cellPosition = gridLayout.WorldToCell(point);
+        map.SetTile(cellPosition, tile);
+    }
+
     public void PressDown()
     {
-        canInstall = contoller.IsTileCanInstall(contoller.root, rootPosition + Vector2.down);
+        canInstall = IsTileCanInstall(root, rootPosition + Vector2.down);
         if (!canInstall) 
             return;
 
@@ -31,21 +57,21 @@ public class Root
         rootPosition += Vector2.down;
 
         // Change Tile
-        contoller.ChangeTile(contoller.root, rootPosition);
-        contoller.ChangeTile(contoller.stem, stemPosition);
+        ChangeTile(root, rootPosition);
+        ChangeTile(stem, stemPosition);
     }
 
     public void PressUp()
     {
-        canInstall = contoller.IsTileCanInstall(contoller.root, rootPosition + Vector2.up);
+        canInstall = IsTileCanInstall(root, rootPosition + Vector2.up);
         if (!canInstall) 
             return;
 
         stemPosition += Vector2.down; 
         rootPosition += Vector2.up;
 
-        contoller.ChangeTile(contoller.root, rootPosition);
-        contoller.ChangeTile(contoller.stem, stemPosition);
+        ChangeTile(root, rootPosition);
+        ChangeTile(stem, stemPosition);
     }
 
     public void PressRight()
@@ -77,44 +103,5 @@ public class Root
     public void Reset()
     {
        
-    }
-}
-
-public class RootContoller : MonoBehaviour
-{
-    public Tile root;
-    public Tile stem;
-    public Tile limit;
-
-    private Tilemap map;
-    private GridLayout gridLayout;
-    private Vector3Int cellPosition;
-
-    public List<Root> roots = new List<Root>();
-
-    private void Start()
-    {
-        map = GetComponent<Tilemap>();
-        gridLayout = transform.parent.GetComponentInParent<GridLayout>();
-
-        foreach(var root in roots)
-        {
-            root.contoller = this;
-        }
-    }
-
-    public bool IsTileCanInstall(Tile tile, Vector2 pos)
-    {
-        Tile target = map.GetTile<Tile>(gridLayout.WorldToCell(pos));
-        if (tile == target || tile == limit)
-            return false;
-        else
-            return true;
-    }
-
-    public void ChangeTile(Tile tile, Vector2 point)
-    {        
-        cellPosition = gridLayout.WorldToCell(point);
-        map.SetTile(cellPosition, tile);
     }
 }
