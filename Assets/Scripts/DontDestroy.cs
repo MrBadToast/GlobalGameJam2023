02d484005a970e9865330c0df46fa96992907168
BadToast <ship2042@gmail.com>
2023-02-05 02:26:23 +0900

    DontDestroy

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DestroyThis()
     {
-        
+        Destroy(gameObject);
     }
 }
